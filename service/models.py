19905540c79ef26d87ff2f83682c0b6ec433f8b4
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-04-07 21:18:55 +0300

    2-й коммит, модели

--- service/models.py
+++ service/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Table(models.Model):
+    """ Модель для выбора столика """
+    class Location(models.TextChoices):
+        """ Класс для выбора расположения столика """
+        HALL = 'HL', 'Зал у окна'
+        TERRACE = 'TR', 'Терраса'
+
+    name = models.IntegerField(verbose_name='Номер столика', unique=True)
+    seats = models.IntegerField(verbose_name='Количество мест', validators=[MinValueValidator(1)])
+    location = models.CharField(max_length=2, choices=Location.choices ,verbose_name='Расположение столика')
+
+    class Meta:
+        verbose_name = 'Столик'
+        verbose_name_plural = 'Столики'
+        ordering = ['name',]
+
+    def __str__(self):
+        return f'{self.name} ({self.seats} seats) - {self.location}'
