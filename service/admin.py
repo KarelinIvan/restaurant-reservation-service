d6d7810549e00bc707a861316ac547c662f94e2d
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-04-07 21:52:03 +0300

    4-й коммит, панель в админке

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from service.models import Table, Reservation
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'seats', 'location')
+    search_fields = ('name',)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'table_id', 'reservation_time', 'duration_minutes')
+    search_fields = ('customer_name',)
